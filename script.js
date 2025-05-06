749072d3ba5ed1680e90f3880cb821d3e525dc96
janjan-coder <133653039+janjan-coder@users.noreply.github.com>
2025-05-06 14:41:50 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -4,7 +4,9 @@ let shoes_men3 = document.getElementById("shoes_men3");
 let shoes_men4 = document.getElementById("shoes_men4");
 let shoes_men5 = document.getElementById("shoes_men5");
 let shoes_men6 = document.getElementById("shoes_men6");
-
+let shoes_men7 = document.getElementById("shoes_men7");
+let shoes_men8 = document.getElementById("shoes_men8");
+let shoes_men9 = document.getElementById("shoes_men9");
 
 shoes_men1.addEventListener("mouseover", function(){
     shoes_men1.style.transform="scale(1.05)";
@@ -54,4 +56,29 @@ shoes_men6.addEventListener("mouseover", function(){
 shoes_men6.addEventListener("mouseout", function(){
     shoes_men6.style.transform="scale(1.00)";
     shoes_men6.style.boxShadow="none";
+})
+shoes_men7.addEventListener("mouseover", function(){
+    shoes_men7.style.transform="scale(1.05)";
+    shoes_men7.style.boxShadow="10px 10px 8px grey";
+})
+shoes_men7.addEventListener("mouseout", function(){
+    shoes_men7.style.transform="scale(1.00)";
+    shoes_men7.style.boxShadow="none";
+})
+shoes_men8.addEventListener("mouseover", function(){
+    shoes_men8.style.transform="scale(1.05)";
+    shoes_men8.style.boxShadow="10px 10px 8px grey";
+})
+shoes_men8.addEventListener("mouseout", function(){
+    shoes_men8.style.transform="scale(1.00)";
+    shoes_men8.style.boxShadow="none";
+})
+
+shoes_men9.addEventListener("mouseover", function(){
+    shoes_men9.style.transform="scale(1.05)";
+    shoes_men9.style.boxShadow="10px 10px 8px grey";
+})
+shoes_men9.addEventListener("mouseout", function(){
+    shoes_men9.style.transform="scale(1.00)";
+    shoes_men9.style.boxShadow="none";
 })
